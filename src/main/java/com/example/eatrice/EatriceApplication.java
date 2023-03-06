06e345c1a11c3b9d22077606d1a989954aa9e899
LAPTOP-S0S4KJ4D\14118 <1411804190@qq.com>
2023-03-06 16:27:10 +0800

    1.init the project,it's a simple version.

---
+++ src/main/java/com/example/eatrice/EatriceApplication.java
@@ -0,0 +1,13 @@
+package com.example.eatrice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatriceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EatriceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/eatrice/EatriceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eatrice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatriceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
